8b41bc06ec737a3e7496e2e59a51732051d4142e
Binit Joshi <binitjoshi4554@gmail.com>
2025-08-29 15:46:14 +0545

    Restart

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [App],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [provideBrowserGlobalErrorListeners(), provideStoreDevtools()],
   bootstrap: [App],
 })

343e3e03587ec6766194009723b847431edb580e
puja lakhe <lakhepuza924@gmail.com>
2025-08-29 13:33:30 +0545

    Refactor MaterialModule to include necessary Angular Material imports and exports for improved functionality

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [],
-  imports: [],
-  providers: [],
+  imports: [MatListModule, MatButtonModule, MatCardModule, MatIconModule],
+  exports: [MatListModule, MatButtonModule, MatCardModule, MatIconModule],
 })
 export class MaterialModule {}

9e2b61dcf1f57cdd0bab06cb4d7b997332a0b2c2
Binit Joshi <binitjoshi4554@gmail.com>
2025-08-29 11:22:32 +0545

    Ngrx initialized

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners(), provideStoreDevtools()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}

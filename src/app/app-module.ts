c5f237f5b43addfa6fe72beb274392a2f2f1aa8e
Binit Joshi <binitjoshi4554@gmail.com>
2025-08-29 16:02:38 +0545

    folder structure created

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './shared/components/header-component/header-component';
+import { SidebarComponent } from './shared/components/sidebar-component/sidebar-component';
 
 @NgModule({
-  declarations: [App],
+  declarations: [App, HeaderComponent, SidebarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [provideBrowserGlobalErrorListeners(), provideStoreDevtools()],
   bootstrap: [App],

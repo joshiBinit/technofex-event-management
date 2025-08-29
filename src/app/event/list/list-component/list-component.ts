dc44443e79d927431d2944ccb0a43f15a3af58a5
puja lakhe <lakhepuza924@gmail.com>
2025-08-29 11:40:38 +0545

    Add List and Dashboard components with basic structure and tests,and add user-module

---
+++ src/app/event/list/list-component/list-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-component',
+  standalone: false,
+  templateUrl: './list-component.html',
+  styleUrl: './list-component.scss'
+})
+export class ListComponent {
+
+}

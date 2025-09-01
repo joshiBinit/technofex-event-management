7f2178e6bda2f7dd0696be52f28d5df12a1b1e20
Binit Joshi <binitjoshi4554@gmail.com>
2025-09-01 16:49:22 +0545

    random

--- src/app/features/private/dashboard/component/dashboard/dashboard.ts
+++ src/app/features/private/dashboard/component/dashboard/dashboard.ts
@@ -31,7 +31,7 @@ export class DashboardComponent implements OnInit {
   displayedColumns: string[] = ['name', 'date', 'location', 'bookings'];
 
   chartOptions: ApexOptions = {
-    series: [], // must always be array
+    series: [],
     chart: { type: 'pie', width: 380 }, // must always be defined
     labels: [], // must always be array
     plotOptions: { pie: { expandOnClick: true } }, // must always be defined

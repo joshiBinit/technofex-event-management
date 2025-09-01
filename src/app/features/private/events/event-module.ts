a6d640789df390e8645aa7c74bb2e91b28e273cc
puja lakhe <lakhepuza924@gmail.com>
2025-09-01 14:37:29 +0545

    event list component

--- src/app/features/private/events/event-module.ts
+++ src/app/features/private/events/event-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EventRoutingModule } from './event-routing-module';
+import { EventListComponent } from './components/event-list-component/event-list-component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EventListComponent
+  ],
   imports: [
     CommonModule,
     EventRoutingModule

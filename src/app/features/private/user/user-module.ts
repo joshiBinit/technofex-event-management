a789b9bafd6c5b3234522275046698c65f1b00ca
puja lakhe <lakhepuza924@gmail.com>
2025-09-01 13:39:49 +0545

    add user related feature routes

--- src/app/features/private/user/user-module.ts
+++ src/app/features/private/user/user-module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing-module';
 import { UserDashboardComponent } from './components/user-dashboard-component/user-dashboard-component';
 import { MaterialModule } from '../../../shared/material.module';
+import { BookingListComponent } from './components/booking-list-component/booking-list-component';
+import { BookingDetailComponent } from './components/booking-detail-component/booking-detail-component';
 
 @NgModule({
-  declarations: [UserDashboardComponent],
+  declarations: [UserDashboardComponent, BookingListComponent, BookingDetailComponent],
   imports: [CommonModule, UserRoutingModule, MaterialModule],
 })
 export class UserModule {}
--- src/app/features/private/user/user-routing-module.ts
+++ src/app/features/private/user/user-routing-module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDashboardComponent } from './components/user-dashboard-component/user-dashboard-component';
+import { BookingListComponent } from './components/booking-list-component/booking-list-component';
+import { BookingDetailComponent } from './components/booking-detail-component/booking-detail-component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: UserDashboardComponent,
+  },
+  {
+    path: 'booking-list',
+    component: BookingListComponent,
+  },
+  {
+    path: 'booking-detail',
+    component: BookingDetailComponent,
   },
 ];
 

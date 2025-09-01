de6bf1fae9a2784539c4a0dd8e91ed572d913c0a
puja lakhe <lakhepuza924@gmail.com>
2025-09-01 14:26:56 +0545

    fetched random events form db.json and display in user dashboard under recommended for you section

--- src/app/features/private/user/components/user-dashboard-component/user-dashboard-component.ts
+++ src/app/features/private/user/components/user-dashboard-component/user-dashboard-component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../../events/services/event-service';
+import { Event } from '../../../../../shared/model/event.model';
 @Component({
   selector: 'app-user-dashboard-component',
   standalone: false,
   templateUrl: './user-dashboard-component.html',
-  styleUrl: './user-dashboard-component.scss'
+  styleUrl: './user-dashboard-component.scss',
 })
-export class UserDashboardComponent {
-
+export class UserDashboardComponent implements OnInit {
+  events: Event[] = [];
+  constructor(private eventService: EventService) {}
+  ngOnInit() {
+    this.eventService
+      .getRandomEvents(3)
+      .subscribe((data) => (this.events = data));
+  }
 }

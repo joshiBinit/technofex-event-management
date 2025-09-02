654b0e0c6679f569b834cceaf662a6a8d996a201
puja lakhe <lakhepuza924@gmail.com>
2025-09-02 13:52:28 +0545

    booked event shown in user dashboard page directly

--- src/app/features/private/user/components/user-dashboard-component/user-dashboard-component.ts
+++ src/app/features/private/user/components/user-dashboard-component/user-dashboard-component.ts
@@ -3,8 +3,10 @@ import { EventService } from '../../../../../core/services/event/event-service';
 import { Event } from '../../../../../shared/model/event.model';
 import * as BookedEventsActions from '../../../events/store/booked-events/booked-events.action';
 import { Store } from '@ngrx/store';
-
+import * as BookedEventActions from '../../../events/store/booked-events/booked-events.action';
 import { BookedEventsState } from '../../../events/store/booked-events/booked-events.reducer';
+import { Observable } from 'rxjs';
+import { selectBookedEvents } from '../../../events/store/booked-events/booked-events.store';
 @Component({
   selector: 'app-user-dashboard-component',
   standalone: false,
@@ -17,13 +19,27 @@ export class UserDashboardComponent implements OnInit {
   constructor(
     private eventService: EventService,
     private store: Store<BookedEventsState>
-  ) {}
+  ) {
+    this.bookedEvents$ = this.store.select(selectBookedEvents);
+  }
   ngOnInit() {
     this.eventService
       .getRandomEvents(3)
       .subscribe((data) => (this.events = data));
+    this.store.dispatch(BookedEventActions.loadBookedEvents());
   }
   onBookNow(event: Event) {
     this.store.dispatch(BookedEventsActions.bookEvent({ event }));
+    alert(`${event.title} added`);
   }
+  bookedEvents$: Observable<Event[]>;
+  displayedColumns: string[] = [
+    'title',
+    'category',
+    'date',
+    'time',
+    'tickets',
+    'price',
+    'actions',
+  ];
 }

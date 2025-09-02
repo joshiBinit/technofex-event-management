891c2c01c85678195d0762b55415deb5009cc255
puja lakhe <lakhepuza924@gmail.com>
2025-09-02 12:35:03 +0545

    add book now event in user dashboard recommend page

--- src/app/features/private/user/components/user-dashboard-component/user-dashboard-component.ts
+++ src/app/features/private/user/components/user-dashboard-component/user-dashboard-component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService } from '../../../events/services/event-service';
+import { EventService } from '../../../../../core/services/event/event-service';
 import { Event } from '../../../../../shared/model/event.model';
+import * as BookedEventsActions from '../../../events/store/booked-events/booked-events.action';
+import { Store } from '@ngrx/store';
+
+import { BookedEventsState } from '../../../events/store/booked-events/booked-events.reducer';
 @Component({
   selector: 'app-user-dashboard-component',
   standalone: false,
@@ -9,10 +13,17 @@ import { Event } from '../../../../../shared/model/event.model';
 })
 export class UserDashboardComponent implements OnInit {
   events: Event[] = [];
-  constructor(private eventService: EventService) {}
+
+  constructor(
+    private eventService: EventService,
+    private store: Store<BookedEventsState>
+  ) {}
   ngOnInit() {
     this.eventService
       .getRandomEvents(3)
       .subscribe((data) => (this.events = data));
+  }
+  onBookNow(event: Event) {
+    this.store.dispatch(BookedEventsActions.bookEvent({ event }));
   }
 }

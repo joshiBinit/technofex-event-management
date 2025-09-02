b9d0077d97a2e9366890018477633c61b8266e4d
puja lakhe <lakhepuza924@gmail.com>
2025-09-02 11:00:01 +0545

    add service to get random events

--- src/app/core/services/event/event-service.ts
+++ src/app/core/services/event/event-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Event } from '../../../shared/model/event.model';
 
 @Injectable({
@@ -15,5 +15,13 @@ export class EventService {
   }
   getEvents(): Observable<Event[]> {
     return this.http.get<Event[]>(this.apiUrl);
+  }
+
+  getRandomEvents(limit: number = 3): Observable<Event[]> {
+    return this.http.get<Event[]>(this.apiUrl).pipe(
+      map((events) => {
+        return events.sort(() => 0.5 - Math.random()).slice(0, limit);
+      })
+    );
   }
 }

9c679ba1b7c6b163e0c8724d8948140fae62b207
puja lakhe <lakhepuza924@gmail.com>
2025-09-02 12:27:46 +0545

    add availableTickets property to event Model

--- src/app/features/private/private-module.ts
+++ src/app/features/private/private-module.ts
@@ -5,9 +5,20 @@ import { PrivateRoutingModule } from './private-routing-module';
 import { SharedLayout } from './shared-layout/shared-layout';
 import { SharedModule } from '../../shared/shared-module';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { StoreModule } from '@ngrx/store';
+import { bookedEventsReducer } from './events/store/booked-events/booked-events.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BookedEventsEffects } from './events/store/booked-events/booked-events.effect';
 
 @NgModule({
   declarations: [SharedLayout],
-  imports: [CommonModule, PrivateRoutingModule, SharedModule, MatSidenavModule],
+  imports: [
+    CommonModule,
+    PrivateRoutingModule,
+    SharedModule,
+    MatSidenavModule,
+    StoreModule.forFeature('bookedEvents', bookedEventsReducer),
+    EffectsModule.forFeature(BookedEventsEffects),
+  ],
 })
 export class PrivateModule {}

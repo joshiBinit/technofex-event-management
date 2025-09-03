c62b46e55eabe75c29c710825a9f3f96caa50dfd
puja lakhe <lakhepuza924@gmail.com>
2025-09-03 13:39:01 +0545

    user service  unit testing done

--- src/app/core/services/users/user-service.spec.ts
+++ src/app/core/services/users/user-service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { UserService } from './user-service';
+import { User } from '../../../shared/model/user.model';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
+    });
+
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should GET users', () => {
+    const mockUsers: User[] = [
+      {
+        username: 'Alice',
+        password: 'pass123',
+        role: 'user',
+        email: 'alice@test.com',
+        bookings: [],
+      },
+      {
+        username: 'Bob',
+        password: 'admin123',
+        role: 'admin',
+        email: 'bob@test.com',
+      },
+    ];
+
+    service.getUsers().subscribe((users) => {
+      expect(users).toEqual(mockUsers);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/users');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
   });
 });

6c00259c4ad1aa8a34860083fad8e2563d72ea5c
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-09-05 11:46:07 -0700

    done small fix

--- src/app/features/private/user/components/booking-detail-component/booking-detail-component.ts
+++ src/app/features/private/user/components/booking-detail-component/booking-detail-component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-<<<<<<< HEAD
-
-=======
->>>>>>> 45c8d0458d3d35b587d0737da029b7c134e20eee
 import { Event } from '../../../../../shared/model/event.model';
 import { EventService } from '../../../../../core/services/event/event-service';
-
-import { EventService } from '../../../../../core/services/event/event-service';
-
 @Component({
   selector: 'app-booking-detail-component',
   standalone: false,
@@ -16,12 +9,7 @@ import { EventService } from '../../../../../core/services/event/event-service';
   styleUrls: ['./booking-detail-component.scss'],
 })
 export class BookingDetailComponent implements OnInit {
-<<<<<<< HEAD
   eventDetail!: Event;
-=======
-  event: Event | null = null;
-
->>>>>>> 45c8d0458d3d35b587d0737da029b7c134e20eee
   constructor(
     private route: ActivatedRoute,
     private eventService: EventService
@@ -29,7 +17,6 @@ export class BookingDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
-<<<<<<< HEAD
       const eventId = params.get('id');
       if (eventId) {
         this.eventService.getEventById(eventId).subscribe({
@@ -38,15 +25,6 @@ export class BookingDetailComponent implements OnInit {
           },
           error: (err) => {
             console.error('Failed to fetch event:', err);
-=======
-      const id = params.get('id');
-      if (id) {
-        this.eventService.getEventById(id).subscribe({
-          next: (data) => (this.event = data),
-          error: (err) => {
-            console.error('❌ Failed to load event', err);
-            this.event = null;
->>>>>>> 45c8d0458d3d35b587d0737da029b7c134e20eee
           },
         });
       }

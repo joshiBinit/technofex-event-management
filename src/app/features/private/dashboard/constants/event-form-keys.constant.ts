2b8de3968c27fa53890478ba44f0ed15d0729ce7
puja lakhe <lakhepuza924@gmail.com>
2025-09-11 09:48:06 +0545

    event form keys constant

---
+++ src/app/features/private/dashboard/constants/event-form-keys.constant.ts
@@ -0,0 +1,13 @@
+export const EVENT_FORM_KEYS = {
+  TITLE: 'title',
+  CATEGORY: 'category',
+  DESCRIPTION: 'description',
+  SCHEDULE: {
+    DATE: 'date',
+    TIME: 'time',
+  },
+  LOCATION: 'location',
+  TOTAL_TICKETS: 'totalTickets',
+  AVAILABLE_TICKETS: 'availableTickets',
+  PRICE: 'price',
+} as const;
